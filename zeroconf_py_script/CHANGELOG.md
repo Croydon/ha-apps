994fa8c55ff01f021fd9386229b3e8a89e234ba6
Croydon <git@cr0ydon.com>
2026-09-22 07:23:28 +0200

    Create the file if it does not exist at startup

--- zeroconf_py_script/CHANGELOG.md
+++ zeroconf_py_script/CHANGELOG.md
@@ -1,1 +1,9 @@
 # Zeroconf Py Script HA App Changelog
+
+## 0.0.2
+
+  * Change `addon_config` config map to `app_config`
+
+## 0.0.1
+
+  * Init
--- zeroconf_py_script/entrypoint.sh
+++ zeroconf_py_script/entrypoint.sh
@@ -7,4 +7,9 @@ with open("/data/options.json") as f:
     print(json.load(f)["ZPS_FILE"])
 ')"
 
+if [ ! -f "$ZPS_FILE" ]; then
+    mkdir -p "$(dirname "$ZPS_FILE")"
+    touch "$ZPS_FILE"
+fi
+
 exec python "$ZPS_FILE"
